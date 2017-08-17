c82cfc903aacf1252b862faa86717819eb64e357
viktorv <viktorvelev8@gmail.com>
2017-08-17 21:55:51 +0300

    Added moving years by 1

--- js/events.js
+++ js/events.js
@@ -24,3 +24,21 @@ $(window).on("DOMContentLoaded", function() {
 $(window).on("mousemove", function() {
     $("#tooltip-container").css({ top: event.clientY - 45, left: event.clientX + 5, position: 'absolute' });
 });
+
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 38) {
+        console.log("Up");
+        currentJSON = parseInt(currentJSON) + 1;
+
+        console.log(currentJSON);
+        $('input[type="range"]').val(currentJSON);
+        $('input[type="range"]').trigger('change');
+    }
+    if (event.keyCode == 40) {
+        console.log("Down");
+        currentJSON -= 1;
+        console.log(currentJSON);
+        $('input[type="range"]').val(currentJSON);
+        $('input[type="range"]').trigger('change');
+    }
+});
